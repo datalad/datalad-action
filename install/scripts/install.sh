cee3b31dc4191af08d0a93ea08d761c32948a742
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-08-13 23:53:24 -0600

    adding datalad install composite action (#1)
    * adding datalad install composite action
    * add workflows to test action
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ install/scripts/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# Show the user all relevant variables for debugging!
+printf "user: ${user}\n"
+printf "email: ${email}\n"
+printf "repository: ${repository}\n"
+printf "release: ${release}\n"
+printf "branch: ${branch}\n"
+printf "full_clone: ${full_clone}\n"
+
+# This is in the install instructions
+git config --global --add user.name "${user}"
+git config --global --add user.email  "${email}"
+
+python -m pip install --upgrade pip
+
+# Do we have a release or a branch?
+if [ "${release}" != "" ]; then
+    printf "Installing datalad from release ${release}...\n"
+    wget https://github.com/${repository}/archive/refs/tags/${release}.tar.gz
+    pip install ${release}.tar.gz
+
+# Branch install, either shallow or full clone
+else
+    if [[ "${full_clone}" == "true" ]]; then
+        printf "Installing datalad from branch ${branch} with full clone...\n"
+        git clone --depth 1 -b ${branch} https://github.com/${repository} /tmp/datalad
+        cd /tmp/datalad
+        pip install .
+        cd -
+        rm -rf /tmp/datalad
+    else
+        printf "Installing datalad from branch ${branch}...\n"
+        pip install git+https://github.com/${repository}.git@${branch}
+    fi
+fi
