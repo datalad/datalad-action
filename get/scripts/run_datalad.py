6ee5d2526d1b4e9803a84fdaacb32e59f6d26cc1
vsoch <vsoch@users.noreply.github.com>
2022-08-14 12:29:17 -0600

    tweaks
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- get/scripts/run_datalad.py
+++ get/scripts/run_datalad.py
@@ -8,22 +8,34 @@ import datalad.api as dl
 true = [True, "true", "True"]
 
 
-def get_listing(key="paths"):
+def get_paths():
     """
-    Derive datasets from the environment.
+    Derive paths (newline separated) from the environment.
     """
-    items = os.environ.get(key)
+    items = os.environ.get("paths")
     if items and isinstance(items, str):
         items = [x.strip() for x in items.split("\n")]
     return items or []
 
+def get_jobs():
+    """
+    Get number of jobs (workers) to download
+    """
+    jobs = os.environ.get("jobs", 1) 
+
+    # The default is a string "auto"
+    try:
+        jobs = int(jobs)
+    except ValueError:
+        pass
+    return jobs
 
 def main():
+    paths = get_paths()
+    jobs = get_jobs()
     source = os.environ.get("source")
-    paths = get_listing("paths")
     recursive = os.environ.get("recursive") in true
     all_data = os.environ.get("all") in true
-    jobs = int(os.environ.get("jobs", 1) or 1)
 
     print("  Dataset: %s" % source)
     print("    Paths: %s" % paths)
